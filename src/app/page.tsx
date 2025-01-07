b940aad5d5e8332dea6e6604c5d8e76be832faa2
travisfriesen <github@travisfriesen.ca>
2025-01-06 20:33:18 -0600

    added Front Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import {FrontCard} from "@/components/FrontCard";
 import {ProfileCard} from "@/components/ProfileCard";
+import {Header} from "@/components/Header";
+import {ProjectCard} from "@/components/ProjectCard";
 
 export default function Home() {
   return (
     <main>
-      <FrontCard/>
-        <ProfileCard/>
+        <div>
+            <FrontCard/>
+            <ProfileCard/>
+        </div>
     </main>
   );
 }
--- src/components/FrontCard.tsx
+++ src/components/FrontCard.tsx
@@ -1,7 +1,7 @@
 export const FrontCard = () => {
     return(
-        <div className={`w-[100vw] h-[100vh] flex`}>
-            <div className="flex bg-green-80 rounded-lg h-[95vh] w-[95vw] m-auto">
+        <div className={`w-[100vw] h-[100vh] min-h-[720px] flex`}>
+            <div className="flex bg-green-80 bg-gradient-to-tr from-green-90 to-green-50 rounded-lg h-[95vh] w-[95vw] min-h-[700px] m-auto">
                 <div className="my-auto ml-[5%]">
                     <p className="text-grey-90 text-4xl">Hi, I'm</p>
                     <h1 className={`text-gold-50 text-[1000%] font-array font-bold`}>TRAVIS</h1>
