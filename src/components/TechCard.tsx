5e723f237f5cb4eabdfe508052b45e0155c25368
travisfriesen <github@travisfriesen.ca>
2025-01-09 08:12:01 -0500

    added tech card

--- src/components/TechCard.tsx
+++ src/components/TechCard.tsx
@@ -5,7 +5,8 @@ interface ITechCard {
 export const TechCard = ({text}: ITechCard) => {
     return(
         <div className={`bg-gold-40 border-[3px] border-gold-80 rounded-xl`}>
-            <p className={`font-manrope font-bold text-gold-95 py-1 px-3 text-xl`}>{text}</p>
+            <p className={`desktop-only font-manrope font-bold text-gold-95 py-1 px-5 text-xl`}>{text}</p>
+            <p className={`mobile-only text-center font-manrope font-bold text-gold-95 py-1 px-3 text-xl`}>{text}</p>
         </div>
     )
 }
