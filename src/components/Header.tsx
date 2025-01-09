fc2958b89530b4949926b623a1847d5bedab7150
travisfriesen <github@travisfriesen.ca>
2025-01-09 08:11:48 -0500

    updated text sizing on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ interface IHeader {
 export const Header = ({text}: IHeader) => {
     return(
         <div className={`mt-14 mb-14 ml-[10%]`}>
-            <h1 className={`font-bold font-manrope text-8xl text-gold-95`}>{text}</h1>
+            <h1 className={`font-bold font-manrope text-gold-95 text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl`}>{text}</h1>
         </div>
-    )
+)
 }
