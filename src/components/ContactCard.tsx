9eda86dac0dd52793a4126517698f9804ac18607
travisfriesen <github@travisfriesen.ca>
2025-01-09 08:11:22 -0500

    added contact card

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,21 @@
+import {FaEnvelope, FaGithub, FaLinkedin} from "react-icons/fa6";
+
+export const ContactCard = () => {
+    return(
+        <div className={`flex flex-col gap-6 py-[5vh]`}>
+            <div className={`m-auto w-[65vw] text-gold-95 text-xl font-manrope font-bold`}>
+                <p>If you are interested in more of my work, I would encourage you to check out my GitHub.</p>
+                <br/>
+                <p>I'm also on LinkedIn, feel free to connect with me there!</p>
+                <br/>
+                <p>If you are interested in contacting me, I would encourage you to email me at
+                    travis[at]travisfriesen.ca or to add me on Discord (@travisfriesen)</p>
+            </div>
+            <div className={`flex m-auto gap-[4vw] py-5`}>
+                <a href={"https://github.com/travisfriesen"}><FaGithub size={60}/></a>
+                <a href={"mailto:travis@travisfriesen.ca"}><FaEnvelope size={60}/></a>
+                <a href={"https://linkedin.com/travisfriesen"}><FaLinkedin size={60}/></a>
+            </div>
+        </div>
+    )
+}
