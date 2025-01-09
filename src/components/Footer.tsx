cdd028c6322d15c90e517b3d6586a764d00f7a14
travisfriesen <github@travisfriesen.ca>
2025-01-09 08:11:11 -0500

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+    return(
+        <div className={`bg-gold-20 h-fit py-6 flex`}>
+            <div className={`w-fit m-auto font-manrope font-bold text-xl px-4`}>
+                <p className={`w-fit`}>Website designed in Figma, created using Next.js and hosted in a closet somewhere.</p>
+                <p>© Travis Friesen</p>
+            </div>
+        </div>
+    )
+}
