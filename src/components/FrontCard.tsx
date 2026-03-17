42da6f2507c2086b1fd9bdf1cf1e1ccf5fcdb9ac
travisfriesen <github@travisfriesen.ca>
2026-03-16 23:34:23 -0500

    fix width issue

--- src/components/FrontCard.tsx
+++ src/components/FrontCard.tsx
@@ -1,17 +1,17 @@
 export const FrontCard = () => {
-    return (
-        <div className={`w-screen h-screen min-h-[720px] flex`}>
-            <div
-                className="flex bg-green-80 dark:bg-green-20 bg-linear-to-tr from-green-90 to-green-50 rounded-lg h-[95vh] w-[95vw] min-h-[700px] m-auto">
-                <div className="my-auto ml-[5%]">
-                    {/* eslint-disable-next-line react/no-unescaped-entities */}
-                    <p className="text-green-5 text-4xl">Hi, I'm</p>
-                    <div className={`text-[300%] sm:text-[600%] md:text-[750%] lg:text-[850%] xl:text-[1000%]`}>
-                        <h1 className={`text-gold-50 font-array font-bold`}>TRAVIS</h1>
-                        <h1 className={`text-gold-50 font-array font-bold`}>FRIESEN</h1>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className={`w-fill h-screen min-h-180 flex`}>
+			<div
+				className="flex bg-green-80 dark:bg-green-20 bg-linear-to-tr from-green-90 to-green-50 rounded-lg h-[95vh] w-[95vw] min-h-175 m-auto">
+				<div className="my-auto ml-[5%]">
+					{/* eslint-disable-next-line react/no-unescaped-entities */}
+					<p className="text-green-5 text-4xl">Hi, I'm</p>
+					<div className={`text-[300%] sm:text-[600%] md:text-[750%] lg:text-[850%] xl:text-[1000%]`}>
+						<h1 className={`text-gold-50 font-array font-bold`}>TRAVIS</h1>
+						<h1 className={`text-gold-50 font-array font-bold`}>FRIESEN</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
