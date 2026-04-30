428e7809e567901160ac5ff1e19d3b33c626df1b
travisfriesen <github@travisfriesen.ca>
2026-04-30 04:23:44 +0000

    fix: grammar (thanks @ediffs) (#12)
    This PR is a copy of this PR https://github.com/travisfriesen/next.travisfriesen.ca/pull/12 brought over to this git instance, for proper ci/cd checks and deployments.
    
    Fixes the grammar on the Profile Card
    
    Reviewed-on: https://git.travisf.ca/travisfriesen/travisfriesen.ca/pulls/12

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -2,28 +2,35 @@ import Image from "next/image";
 import travisImg from "../../public/travis.png";
 
 export const ProfileCard = () => {
-    return (
-        <div>
-            <div className={`grid desktop:grid-cols-2 mobile:grid-rows-[auto_auto] m-auto w-[95vw] h-fit py-[5%]`}>
-                <div className={`my-auto`}>
-                    <Image className={`rounded-3xl m-auto`}
-                           src={travisImg}
-                           alt="Photo of Travis Friesen"
-                           width={500}
-                           height={500}
-                    />
-                </div>
-                <div className={`font-manrope font-bold text-xl my-auto text-gold-95 dark:text-gold-5 ml-4 mobile:mt-7`}>
-                    <p>
-                        I am a student working towards a degree in Computer Science University of Manitoba.
-                    </p>
-                    <br/>
-                    <p>
-                        Currently I’m working on a few websites, along with some Discord bots and a few other projects
-                        that I’m cooking up.
-                    </p>
-                </div>
-            </div>
+  return (
+    <div>
+      <div
+        className={`grid desktop:grid-cols-2 mobile:grid-rows-[auto_auto] m-auto w-[95vw] h-fit py-[5%]`}
+      >
+        <div className={`my-auto`}>
+          <Image
+            className={`rounded-3xl m-auto`}
+            src={travisImg}
+            alt="Photo of Travis Friesen"
+            width={500}
+            height={500}
+          />
         </div>
-    )
-}
+        <div
+          className={`font-manrope font-bold text-xl my-auto text-gold-95 dark:text-gold-5 ml-4 mobile:mt-7`}
+        >
+          <p>
+            I am a student working towards a degree in Computer Science at the
+            University of Manitoba.
+          </p>
+          <br />
+          <p>
+            Currently I’m working on a few websites, along with some Discord
+            bots and a few other projects that I’m cooking up.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
